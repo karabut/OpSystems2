d6f1bf5629245caa96081bb7e367236d8b0fb137
karabut <kes_n@mail.ru>
2020-12-25 11:07:21 +0700

    initial

--- robots/tests.cpp
+++ robots/tests.cpp
@@ -1,3 +1,7 @@
+//
+// Created by Katya on 01.12.2020.
+//
+
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
 
@@ -8,6 +12,6 @@ class MemoryTest: public testing::Test {};
 class TimeTests: public testing::Test {};
 
 TEST_F(CommandTest, steps) {
-    ASSERT_EQ(5, 5);
+    ASSERT_EQ(6, 6);
 }
 
